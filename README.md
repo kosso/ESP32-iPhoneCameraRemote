4d1a578a0a7f09273212e630bf5d378c3a17ae11
Kosso <kosso1@gmail.com>
2020-10-17 23:11:39 +0100

    Add files

---
+++ README.md
@@ -0,0 +1,29 @@
+# ESP32-iPhoneCameraRemote
+
+------
+
+A very simple ESP32 demo application which sends the Volume up key when the boot button is pressed (when paired to an iOS device). 
+
+The volume keys can be used on an iPhone to take a photo in the Camera app.
+
+### HowTo
+
+- See: `BleKeyboard` initialisation in `main.cpp` for naming options.
+- Open Bluetooth settings on the the iPhone and pair with the device. 
+- Open Camera app.
+- Say cheese.
+- Click the ESP32 Boot button.
+
+-----
+
+Tested on an iPhoneXR on iOS14.
+
+Uses the excellent ESP32-BLE-Keyboard library from @T-vk
+
+https://github.com/T-vK/ESP32-BLE-Keyboard
+
+----
+
+
+@kosso 2020
+
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+// @kosso 2020. 
+
+// Uses the ESP32-BLE-Keyboard library from https://github.com/T-vK/ESP32-BLE-Keyboard 
+
+#include <BleKeyboard.h>
+
+BleKeyboard bleKeyboard("KOSSP32", "Kosso", 80); // Name, Manufacturer, battery level (apparently cannot be changed. )
+
+#define BTN_PIN 0 // boot button
+
+void sendVolumeUp(){
+
+  if(bleKeyboard.isConnected()) {
+
+    Serial.println("Sending volume up key ...");
+    bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+    delay(100);
+    bleKeyboard.releaseAll();
+  } else {
+    Serial.println("not connected ...");
+  }
+
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE");
+  bleKeyboard.begin();
+
+  // Use an interrupt to detect the Boot button press (not release. Use 'RISING' for that).
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN), sendVolumeUp, FALLING);  
+
+}
+
+
+void loop() {
+
+  delay(100);
+  // .. 
+}
